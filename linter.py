3fcee1c6a5912096e5a6842bb09573d0bbe1b21e
cmc <cr.rey.clement@gmail.com>
2014-04-22 21:06:24 +0200

    linter class (flake8 and pep257 compliant)

--- linter.py
+++ linter.py
@@ -1,21 +1,56 @@
 ###
 # Erlang linter plugin from SublimeLinter3
-# Using escript, make sure it is in your PATH
+# Uses erlc, make sure it is in your PATH
 #
 # Copyright (C) 2014  Clement 'cmc' Rey <cr.rey.clement@gmail.com>
 #
 # MIT License
 ###
 
-"""
-This module exports the Escript plugin class
-"""
+"""This module exports the Erlc plugin class."""
 
 import shlex
-from SublimeLinter.lint import Linter, util, persist
+from SublimeLinter.lint import Linter, util
 
 
 class Erlc(Linter):
-    """
-    Provides an interface to escript
-    """
+
+    """Provides an interface to escript."""
+
+    syntax = (
+        "erlang",
+        "erlang improved"
+    )
+
+    cmd = None
+    executable = "erlc"
+    tempfile_suffix = "erl"
+
+    # ERROR FORMAT # <file>:<line>: [Warning:|] <message> #
+    regex = (
+        r".+:(?P<line>\d+):"
+        r"(?:(?P<warning>\sWarning:\s)|(?P<error>\s))"
+        r"+(?P<message>.+)"
+    )
+
+    error_stream = util.STREAM_STDOUT
+
+    defaults = {
+        "include_dirs": []
+    }
+
+    def cmd(self):
+        """
+        return the command line to execute.
+
+        this func is overridden so we can handle included directories.
+        """
+        ret = "erlc -W"
+
+        settings = self.get_view_settings()
+        dirs = settings.get('include_dirs', [])
+
+        if dirs:
+            ret += " " + " ".join(["-I" + shlex.quote(i) for i in dirs])
+
+        return ret
